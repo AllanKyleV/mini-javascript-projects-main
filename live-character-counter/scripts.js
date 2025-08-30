fae431d85c056c8a5395a29a07000dadac011f20
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-08-30 10:02:46 +0800

    all done

--- live-character-counter/scripts.js
+++ live-character-counter/scripts.js
@@ -2,6 +2,8 @@
 const count = document.querySelector('#count');
 const maxCount = document.querySelector('#max-count');
 const textArea = document.querySelector('#message');
+const maxInput = document.querySelector('#input-max')
+const updateBtn = document.querySelector('#update-btn');
 
 function incrementCount(text) {
     return text.length;
@@ -11,10 +13,39 @@ function decrementCount(max, text) {
     return max - text.length;
 }
 
-textArea.addEventListener('input', () => {
-    let num = incrementCount(textArea.value);
-    count.textContent = num;
+function maxLimit() {
+    const val = Number(maxInput.value);
+    return maxInput.value === '' || isNaN(val) ? 150 : val;
+}
 
-    let maxNum = decrementCount(1500, textArea.value);
-    maxCount.textContent = maxNum;
+// Default limit
+let limit = maxLimit();
+count.textContent = incrementCount(textArea.value);  
+maxCount.textContent = decrementCount(limit, textArea.value);
+
+// Update limit when button clicked
+updateBtn.addEventListener('click', () => {
+    limit = maxLimit();
+    maxCount.textContent = decrementCount(limit, textArea.value);
+});
+
+// Text area input
+textArea.addEventListener('input', () => {
+
+    if (textArea.value.length > limit) {
+        textArea.value = textArea.value.slice(0, limit);
+    }
+
+    count.textContent = incrementCount(textArea.value);
+    maxCount.textContent = decrementCount(limit, textArea.value);
+
+    if (decrementCount(limit, textArea.value) <= limit * 0.20) {
+        maxCount.style.color = 'red';
+    } else {
+        maxCount.style.color = 'black';
+    }
+
+    if (decrementCount(limit, textArea.value) === 0) {
+        maxCount.textContent = "Limit reached!";
+    }
 });
